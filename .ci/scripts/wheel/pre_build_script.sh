6a8ad3e6e611b514ff6070c8e4ab9980364a57b0
Chizkiyahu Raful <chizkiyahuraful@gmail.com>
2025-09-10 22:09:33 +0300

    Add GitHub Actions workflow for building Aarch64 Linux wheels

--- .ci/scripts/wheel/pre_build_script.sh
+++ .ci/scripts/wheel/pre_build_script.sh
@@ -9,6 +9,18 @@ set -euxo pipefail
 
 # This script is run before building ExecuTorch binaries
 
+if [[ "$(uname -m)" == "aarch64" ]]; then
+  # On some Linux aarch64 systems, the "atomic" library is not found during linking.
+  # To work around this, replace "atomic" with the literal ${ATOMIC_LIB} so the
+  # build system uses the full path to the atomic library.
+  file="extension/llm/tokenizers/third-party/sentencepiece/src/CMakeLists.txt"
+  sed 's/list(APPEND SPM_LIBS "atomic")/list(APPEND SPM_LIBS ${ATOMIC_LIB})/' \
+    "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
+
+  grep -n 'list(APPEND SPM_LIBS ${ATOMIC_LIB})' "$file" && \
+    echo "the file $file has been modified for atomic to use full path"
+fi
+
 # Manually install build requirements because `python setup.py bdist_wheel` does
 # not install them. TODO(dbort): Switch to using `python -m build --wheel`,
 # which does install them. Though we'd need to disable build isolation to be
