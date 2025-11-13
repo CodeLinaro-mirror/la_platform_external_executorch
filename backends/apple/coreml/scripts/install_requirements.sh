7602565d581cb2a27efd0518658de70818c52070
Scott Roy <161522778+metascroy@users.noreply.github.com>
2025-11-13 15:11:09 -0800

    Update install_requirements.sh

--- backends/apple/coreml/scripts/install_requirements.sh
+++ backends/apple/coreml/scripts/install_requirements.sh
@@ -12,7 +12,8 @@ SCRIPT_DIR_PATH="$(
 
 # TODO(jathu): remove the need to fetch coremltools to build deps for coreml_executor_runner.
 # Keep this version in sync with: pyproject.toml
-COREMLTOOLS_VERSION="9.0"
+COREMLTOOLS_VERSION="9.0b"
+
 
 red=`tput setaf 1`
 green=`tput setaf 2`
